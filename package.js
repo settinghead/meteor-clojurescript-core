b397965ca7d3365ff2594d91187d8f1671534ba7
Xiyang Chen <settinghead@gmail.com>
2015-11-02 21:23:03 -0500

    Add version constraint for cljs-compiler

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-	api.imply(['settinghead:cljs-compiler']);
+	api.imply(['settinghead:cljs-compiler@0.0.1']);
 	api.addFiles('goog.js', ['server', 'client']);
 	api.addFiles('cljs.core.js', ['server', 'client']);
 	api.export('goog');
